cfaf2cb6ef8fd8a12c2eed705898e95d24c59ab5
João Emanuel <143614248+joao-emanuel-melo@users.noreply.github.com>
2026-03-17 22:54:02 +0000

    Exercicio iniciado em sala

---
+++ Exercicio_aula/Funcionario.java
@@ -0,0 +1,43 @@
+public class Funcionario{
+    private String nome;
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome= nome;
+    }
+
+    private double salario;
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+
+    int TotalFuncionarios;
+
+    public int getTotalFuncionarios(){
+        return TotalFuncionarios;
+    }
+
+    public void setTotalFuncionarios(int TotalFuncionarios){
+        this.TotalFuncionarios = TotalFuncionarios;
+    }
+
+    String ID;
+
+    public String getID(){
+        return ID;
+    }
+
+    public void setID(String ID){
+        this.ID = ID;
+    }
+}
+
+
